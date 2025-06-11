3e492648082f6db20bc5d67222472bc4dcf87d29
owenskydev <evgenykarpovsky1@gmail.com>
2025-06-11 22:04:12 +0300

    stable v0.1

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,15 @@
+import os
+from dotenv import load_dotenv
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler
-from bot.config import BOT_TOKEN
 from bot.handlers import start, button_handler
 
-def main():
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(button_handler))
 
     app.run_polling()
-
-if __name__ == "__main__":
-    main()
